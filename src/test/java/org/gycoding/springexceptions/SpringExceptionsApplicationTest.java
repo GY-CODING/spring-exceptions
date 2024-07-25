e9f52e6d3398410b41ea78af95d7693d35120c46
srtoxyc <ivi1000vm@gmail.com>
2024-07-25 17:09:34 +0200

    fix: test added

---
+++ src/test/java/org/gycoding/springexceptions/SpringExceptionsApplicationTest.java
@@ -0,0 +1,13 @@
+package org.gycoding.springexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExceptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
