5248e25ff2e08837f40e1d58c0aea94d50069e18
srtoxyc <ivi1000vm@gmail.com>
2024-07-30 22:19:38 +0200

    fix: try to fix wrong exception response.

--- src/main/java/org/gycoding/springexceptions/controller/GeneralAdvice.java
+++ src/main/java/org/gycoding/springexceptions/controller/GeneralAdvice.java
@@ -5,16 +5,19 @@ import org.gycoding.springexceptions.model.APIException;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GeneralAdvice {
     @ExceptionHandler({APIException.class})
     public ResponseEntity<String> handleServiceExceptions(APIException ex) throws APIException {
-        return new ResponseEntity<String>(ex.toString(), HttpStatusCode.valueOf(ex.getStatus()));
+        System.out.println(ex.toString());
+        return new ResponseEntity<>(ex.toString(), HttpStatusCode.valueOf(ex.getStatus()));
     }
 }
