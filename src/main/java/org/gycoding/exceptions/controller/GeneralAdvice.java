2b4f9e40324bdc8f85bb44844183fcd6138b760d
ToxYc <ivi1000vm@gmail.com>
2025-01-16 19:50:25 +0100

    fix: remove gy logs

--- src/main/java/org/gycoding/exceptions/controller/GeneralAdvice.java
+++ src/main/java/org/gycoding/exceptions/controller/GeneralAdvice.java
@@ -1,32 +1,16 @@
 package org.gycoding.exceptions.controller;
 
 import org.gycoding.exceptions.model.APIException;
-import org.gycoding.logs.application.service.LogService;
-import org.gycoding.logs.model.LogEntity;
-import org.gycoding.logs.model.LogLevel;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.OffsetDateTime;
-import java.util.UUID;
-
 @RestControllerAdvice
 public class GeneralAdvice {
-    @Value("${gy.logs.origin}")
-    private final String origin = "";
 
     @ExceptionHandler({APIException.class})
     public ResponseEntity<String> handleServiceExceptions(APIException ex) {
-        LogService.log(LogEntity.builder()
-                .id(UUID.randomUUID().toString())
-                .message(ex.getMessage())
-                .level(LogLevel.ERROR)
-                .origin(origin)
-                .timestamp(OffsetDateTime.now().toString())
-                .build());
         return new ResponseEntity<>(ex.toString(), HttpStatusCode.valueOf(ex.getStatus()));
     }
 }
